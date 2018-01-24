87cb1541ad79848e1a345184cc4a297bb5153a04
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-24 21:38:27 +0000

    Setup of a react app using create-react-app command

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ProjectItem extends Component {
+  handleDeleteRequest(title){
+    this.props.onDelete(title)
+  }
+  render() {
+    return(
+      <li>
+        {this.props.project.title} - {this.props.project.type} <button onClick={this.handleDeleteRequest.bind(this, this.props.project.title)}>x</button>
+      </li>
+    );
+  };
+};
+
+export default ProjectItem
