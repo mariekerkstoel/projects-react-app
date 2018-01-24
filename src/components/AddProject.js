705195ca01945fe89514e6ea79adbba23fad28ef
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-24 21:31:43 +0000

    AddProject file that contain addproject component
    Contains state, handlesubmit function that changes the state whenever form is submitted. Addproject renders form with two inputs: title and category.

---
+++ src/components/AddProject.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+
+class AddProject extends Component {
+  constructor(){
+    super()
+    this.state = {
+      newProject: {}
+    }
+  }
+  static defaultProps = {
+    categories: ['boy','girl']
+  };
+
+  HandleSubmit(e) {
+      this.setState({
+        newProject: {
+          title: this.refs.title.value,
+          type: this.refs.category.value
+        }}, function(){
+          this.props.addProject(this.state.newProject)
+        }
+      );
+    e.preventDefault();
+  };
+
+  render(){
+    let CategoryOptions = this.props.categories.map(category => {
+      return <option key={category}>{category}</option>
+    });
+    return(
+      <form onSubmit={this.HandleSubmit.bind(this)}>
+        <div>
+        <label>Title</label><br />
+        <input type='text' ref='title' />
+        </div>
+        <div>
+        <label>Category</label><br />
+        <select ref='category'>
+        {CategoryOptions}
+        </select>
+        </div>
+        <input type='submit' value='Submit' />
+      </form>
+    )
+  };
+
+};
+
+export default AddProject;
