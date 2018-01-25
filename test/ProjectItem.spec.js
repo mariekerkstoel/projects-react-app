b6f652fae30f5fb03869e3d6ea5f6f5dfe8f63b1
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-25 11:15:34 +0000

    Setup testing framework, enzyme, mocha, chai, babel, sinon

---
+++ test/ProjectItem.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProjectItem from '.././src/components/ProjectItem'
+
+describe('a passing test', () => {
+  it('should pass', () => {
+    expect(true).to.be.true;
+  });
+});
